f5e2f24ffa87ea66554b6721a474b42bea7b61f0
venesa okuna <venesa.okuna@student.moringaschool.com>
2022-02-06 13:37:54 +0300

    add file structure

---
+++ app/main/views.py
@@ -0,0 +1,15 @@
+from flask import render_template,request,redirect,url_for,abort
+from . import main
+
+# Views
+@main.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    title = 'ideaBetter'
+
+    return render_template('index.html', title = title )
+
